434604e314c130eaa7f05c05f974af40a2a95480
HarveyCR <lbf.asus.k00z.2014@gmail.com>
2022-10-03 21:27:24 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,17 +1,19 @@
 class Player:
     def __init__(self):
-        self.name = "Main Hero"
+        self.name = "Main_Hero"
         self.age = "21"
         self.guild = "assassin"
         self.existence = "30"
+        self.weapon = "sword"
 
 
 class Enemy:
     def __init__(self):
-        self.name = "Proto Angelo"
+        self.name = "Proto_Angelo"
         self.age = "6528"
         self.guild = "knight"
         self.existence = "500"
+        self.weapon = "sword"
 
 
 class Treasure:
@@ -21,9 +23,13 @@ class Treasure:
 
 
 def main():
-    a = Player()
-    a.name = "Данте"
-    print(a.name)
+    dante = Player()
+    virgil = Player()
+    dante.name = "Данте"
+    virgil.name = "Вергилий"
+    dante.weapon = "soparda_sword"
+    virgil.weapon = "yamato"
+    print(dante.name, virgil.name)
 
 
 if __name__ == '__main__':
