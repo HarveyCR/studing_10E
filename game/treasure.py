acee48f9d860b5f259fad15068fd491f47644cea
HarveyCR <lbf.asus.k00z.2014@gmail.com>
2022-10-03 23:10:22 +0300

    Translation

--- game/treasure.py
+++ game/treasure.py
@@ -6,12 +6,12 @@ def treasure():
     golden_spheres = game.Treasure()
     purple_spheres = game.Treasure()
     blue_spheres = game.Treasure()
-    blood_coins.kind = "blood_coins"
-    golden_spheres.kind = "golden_spheres"
-    purple_spheres.kind = "purple_spheres"
-    blue_spheres.kind = "blue_spheres"
-    blood_coins.ability = "nothing"
-    golden_spheres.ability = "resurrection"
-    purple_spheres.ability = "more the shape of a demon"
-    blue_spheres.ability = "more existence"
-    return blue_spheres, golden_spheres, purple_spheres, blue_spheres
+    blood_coins.kind = "Кровавые монетки"
+    golden_spheres.kind = "Золотые сферы"
+    purple_spheres.kind = "Фиолетовые сферы"
+    blue_spheres.kind = "Синие сферы"
+    blood_coins.ability = "Никаких"
+    golden_spheres.ability = "Воскрешение"
+    purple_spheres.ability = "Увеличение полоски демона"
+    blue_spheres.ability = "Увеличение ХП"
+    return blood_coins, golden_spheres, purple_spheres, blue_spheres
